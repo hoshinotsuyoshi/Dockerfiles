ff83f4db9dacdce15f9e29d06229667193257405
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-05-17 20:39:33 +0900

    Rename container

--- jenkins-docker/start.sh
+++ jenkins-docker/start.sh
@@ -1,1 +1,1 @@
-docker run -d --name ubuntu-jenkins --privileged -p 8080:8080 -v /var/log/.jenkins:/root/.jenkins:rw hoshinotsuyoshi/jenkins-docker
+docker run -d --name jenkins-docker --privileged -p 8080:8080 -v /var/log/.jenkins:/root/.jenkins:rw hoshinotsuyoshi/jenkins-docker
